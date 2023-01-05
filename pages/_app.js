6660dd4327e2859655dc937f282d99f6d53a24cf
Jian Wang <jianwang2010@gmail.com>
2023-01-05 00:26:54 -0500

    Added table to home page

--- pages/_app.js
+++ pages/_app.js
@@ -10,11 +10,11 @@ import Layout from "../components/Layout"
 // Initial setup instructed by Amplify studio
 import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
-import awsconfig from '../src/aws-exports';
+import awsmobile from '../src/aws-exports';
 import "@aws-amplify/ui-react/styles.css";
 import { studioTheme } from "../src/ui-components";
 
-Amplify.configure(awsconfig);
+Amplify.configure({...awsmobile, ssr: true});
 // end initial setup instructed by Amplify studio
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,80 @@
-import React from 'react'
 import Head from "next/head";
 import Link from "next/link"
 // import Image from "next/image"
 import styles from '../styles/Home.module.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Layout from "../components/Layout";
+// import Navbar from '../components/Navbar'
+// import Footer from '../components/Footer'
+// import Layout from "../components/Layout";
 // import { Flex, Box, Text, Button, StylesProvider } from '@chakra-ui/react'
 // import { useRouter } from 'next/router';
 // import { useState, useEffect } from 'react'
+import { withSSRContext } from 'aws-amplify'
+import { listRaces } from '../src/graphql/queries'
+import DataTable from 'react-data-table-component'
+
+const columns = [
+  {
+    name: 'First Name',
+    selector: row => row.firstName,
+    sortable: true,
+  },
+  {
+    name: 'Last Name',
+    selector: row => row.lastName,
+    sortable: true,
+  },
+  {
+    name: 'Finish Hour',
+    selector: row => row.finishHour,
+    sortable: true,
+  },
+  {
+    name: 'Minute',
+    selector: row => row.finishMin,
+    sortable: true,
+  },
+  {
+    name: 'Second',
+    selector: row => row.finishSec,
+    sortable: true,
+  },
+  {
+    name: 'Gender',
+    selector: row => row.gender,
+    sortable: true,
+  },
+  {
+    name: 'Race Name',
+    selector: row => row.raceName,
+    sortable: true,
+  }
+];
+
+export async function getServerSideProps () {
+  const SSR = withSSRContext()
+  const { data } = await SSR.API.graphql({ query: listRaces })
+  return {
+    props: {
+      races: data.listRaces.items
+    }
+  }
+}
 
 
-
-function Home() {
+function Home({races}) {
   return (
     <>
       <Head>
         <title>Marathon Ranking | Home</title>
-        <meta name="keywords" content="Ranking Marathon Finishing Time" />
+        <meta name="keywords" content="Ranking Marathon Finishing Time - All" />
       </Head>
       <div>
         <h1 className={styles.title}>All Runner Finish Time Ranking</h1>
-        <p className={styles.text}>Occaecat deserunt eu id proident consectetur deserunt nostrud fugiat quis voluptate labore cillum. Commodo elit proident mollit sint culpa proident fugiat excepteur nisi nulla anim. Nostrud incididunt nulla reprehenderit officia amet sit elit cillum aliquip amet nostrud mollit. Nisi do enim tempor Lorem.</p>
-        
+        <div className='container'>
+           <h1>Ranking</h1>
+            <DataTable columns={columns} data={races} />
 
+        </div>
         Please go to <Link className='linkover' href='/entry'>Join to Rank</Link> page to enter your race information.
 
       </div>
